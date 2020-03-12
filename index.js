d0eb465afd2ed57131a1cab65340728468ba2478
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-12 18:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
   return obj;
 }
 
-function deleteFromObjectsByKey (obj, key){
+function deleteFromObjectByKey (obj, key){
   var newObj = Object.assign({}, obj);
   delete newObj.key;
   return newObj;
