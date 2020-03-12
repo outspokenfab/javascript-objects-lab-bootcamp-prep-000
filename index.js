c669d6d670a2e9710f5e79c420ac7ef3bbb7dbce
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-12 18:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return newObj;
 }
 
-destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value){
   obj[key]=value;
   return obj;
 }
